3aaa73faac5540142a3f001906ae39f20b2a3510
zvenshy <zvenshy@gmail.com>
2014-07-26 16:05:30 +0800

    theme
    Signed-off-by: zvenshy <zvenshy@gmail.com>

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+/* EcjtuNet u30 theme */
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <title>U30</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <div class="meny">
+        <h2>EcjtuNet U30</h2>
+        <ul>
+        	<li><a href="#">Home</a></li>
+            <li><a href="http://www.ecjtu.net">EcjtuNet</a></li>
+            <li><a href="http://post.ecjtu.net">Post</a></li>
+            <li><a href="http://swzl.ecjtu.net">Swzl</a></li>
+            <li><a href="http://u.ecjtu.net">UCloud</a></li>
+            <li><a href="http://pic.ecjtu.net">Picture</a></li>
+        </ul>
+    </div>
+    <div class="meny-arrow"></div>
+    <div class="contents">
+        <div id="content">
+            <div class="main">
+                <div class="main-wrap">
+                    <div id="logo">日新U30</div>
+                    <div id="picin" class="shake shake-hard">
+                        <div id="year">2014</div>
+                        <div id="theme">该干嘛干嘛，没有css真作死</div>
+                    </div>
+                    <div id="nav"> 
+                        <ul>
+                            <li class="item-1"><a href="">体验设计</a></li>
+                            <li class="item-2"><a href="">网站建设</a></li>
+                            <li class="item-3"><a href="">安全运维</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="footer">
+            <div>
+                <p>
+                    <span id="copyright">CopyRight 2001-2014 By [ecjtu.net] .All Rights Reserved</span>
+                    <a id="about" href="http://hr.ecjtu.net">关于我们</a>
+                    <a id="hr" href="http://about.ecjtu.net">加入我们</a>
+                </p>
+            </div>
+        </div>
+    
+    </div>
+    <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="js/meny.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
