9a6e89e6a464b2ed018544a7e28c0d291782a8c3
zvenshy <zvenshy@gmail.com>
2016-04-10 19:03:54 +0800

    fix
    add footer links

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
     <footer id="footer">
-        <p id="link"><a href="">关于我们</a>|<a href="">网站地图</a>|<a href="">人才招聘</a>|<a href="">日新维基</a></p>
+        <p id="link">
+            <a href="http://www.ecjtu.net/about/">关于我们</a>|
+            <a href="http://123.ecjtu.net/">网站地图</a>|
+            <a href="http://hr.ecjtu.net/">人才招聘</a>|
+            <a href="http://wiki.ecjtu.net/">日新维基</a>
+        </p>
         <p class="copyright">华东交通大学团委、学工处 [<a href="">版权所有 交大日新</a>] 赣ICP备05003322号 日新工作室 维护</p>
         <p class="copyright">Copyright 2001-2014 By [<a href="">ecjtu.net</a>].All Rights Reserved</p>
     </footer>
