ba55a69dd84c3aabb3437110eb36be838ad657d8
zvenshy <zvenshy@gmail.com>
2016-04-14 13:54:51 +0800

    add评论样式
    发表评论的样式还没写；
    顺带添加了标题hover样式

--- comments.php
+++ comments.php
@@ -6,7 +6,7 @@
 <div>
     <div>
         <?php if(post_password_required()): ?>
-            <p>请登陆先！ </p>
+            <p><a href='http://u30.ecjtu.net/register'>请登陆先！</a> </p>
         <?php return;
             endif;?>
     </div>
@@ -17,22 +17,22 @@
             <?php
                 wp_list_comments(array(
                     'style'=> 'ol',
-                    'avatar_size'=> 34,
+                    'avatar_size'=> 60,
                 ));
             ?>
         </ol>
+        <?php endif; ?>
     </div>
 
     <div>
         <?php if(! comments_open()): ?>
         <p> 暂时不开放评论功能 </p>
         <?php endif; ?>
-        <?php endif; ?>
     
         <?php if(is_user_logged_in()){
             comment_form();
-        }else{
-            echo "<p>登陆后才能评论啊！</p>";
+        } else {
+            echo "<p><a href='http://u30.ecjtu.net/register'>登陆</a>后才能评论啊！</p>";
         } 
         ?>
     </div>
