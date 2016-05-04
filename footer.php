7720f7ed000148b0cfc44e4a902b7a804041a0ac
homker <liu15jia@163.com>
2016-05-04 09:53:30 +0800

    fix footer

--- footer.php
+++ footer.php
@@ -6,8 +6,8 @@
             <a href="http://wiki.ecjtu.net/">日新维基</a>|
             <a href="http://u30.ecjtu.net/wp-signup.php">注册</a>
         </p>
-        <p class="copyright">华东交通大学团委、学工处 [<a href="">版权所有 交大日新</a>] 赣ICP备05003322号 日新工作室 维护</p>
-        <p class="copyright">Copyright 2001-2014 By [<a href="">ecjtu.net</a>].All Rights Reserved</p>
+        <p class="copyright">华东交通大学日新 [<a href="">版权所有 交大日新</a>] 赣ICP备05003322号 日新工作室 维护</p>
+        <p class="copyright">Copyright 2001-2016 By [<a href="">ecjtu.net</a>].All Rights Reserved</p>
     </footer>
 </body>
 </html>
