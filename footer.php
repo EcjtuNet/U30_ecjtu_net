e9db05171c5b0c81ee55b0b54d9da4b427ee80c6
homker <liu15jia@163.com>
2016-05-04 09:49:00 +0800

    fix

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
             <a href="http://123.ecjtu.net/">网站地图</a>|
             <a href="http://hr.ecjtu.net/">人才招聘</a>|
             <a href="http://wiki.ecjtu.net/">日新维基</a>|
-            <a href="http://u30.ecjtu.net/register/">Register</a>
+            <a href="http://u30.ecjtu.net/wp-signup.php">注册</a>
         </p>
         <p class="copyright">华东交通大学团委、学工处 [<a href="">版权所有 交大日新</a>] 赣ICP备05003322号 日新工作室 维护</p>
         <p class="copyright">Copyright 2001-2014 By [<a href="">ecjtu.net</a>].All Rights Reserved</p>
